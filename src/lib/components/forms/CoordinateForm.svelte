c67d3cc60cc33ccd47dbb981263d9a907153fc79
Emac <ezekiel342007@gmail.com>
2023-09-24 16:33:00 +0100

    changes to Coordinate form and Card component

--- src/lib/components/forms/CoordinateForm.svelte
+++ src/lib/components/forms/CoordinateForm.svelte
@@ -1,18 +1,30 @@
 <script lang="ts">
+	import Card from '../Card.svelte';
 	import { Input } from '.';
 
 	let latDegRef: HTMLInputElement | null;
 </script>
 
-<div class="p-10 grid grid-cols-1 md:grid-cols-2">
-	<div class="grid grid-cols-3 gap-2">
-		<Input bind:ref={latDegRef} class="w-16 text-xl" type="number" placeholder="9°" />
-		<Input class="w-16 text-xl" type="number" placeholder="00'" />
-		<Input class="w-16 text-xl" type="number" placeholder={'00"'} />
-	</div>
-	<div class="grid grid-cols-3 gap-2">
-		<Input class="w-16 text-xl" type="number" placeholder="9°" />
-		<Input class="w-16 text-xl" type="number" placeholder="00'" />
-		<Input class="w-16 text-xl" type="number" placeholder={'00"'} />
-	</div>
+<div class="grid grid-cols-1 md:grid-cols-2">
+	<Card>
+		<p class="text-lg">Longitude</p>
+		<div class="p-2 flex justify-around">
+			<Input
+				bind:ref={latDegRef}
+				class="!m-2 rounded-md w-16 text-xl"
+				type="number"
+				placeholder="9°"
+			/>
+			<Input class="!m-2 rounded-md w-16 text-xl" type="number" placeholder="00'" />
+			<Input class="!m-2 rounded-md w-16 text-xl" type="number" placeholder={'00"'} />
+		</div>
+	</Card>
+	<Card>
+		<p class="text-lg">Latitude</p>
+		<div class="flex justify-around">
+			<Input class="!m-2 rounded-md w-16 text-xl" type="number" placeholder="9°" />
+			<Input class="!m-2 rounded-md w-16 text-xl" type="number" placeholder="00'" />
+			<Input class="!m-2 rounded-md w-16 text-xl" type="number" placeholder={'00"'} />
+		</div>
+	</Card>
 </div>
